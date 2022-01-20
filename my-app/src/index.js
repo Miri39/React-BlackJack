917f32760460e1ac877ea37e185fd4b02368df92
Miri <mirialin.39@gmail.com>
2022-01-20 15:26:09 +0200

    Status Skeleton

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+let money = 150;
+let cards = Array(53).fill(0);
+
+const Status = () =>{
+    let bet = 0;
+    let unit = 15;
+
+    return(
+        <div style={{width: "94vw", height:"300px", textAlign: "center", margin: "3rem" }}>
+            Money ${money}
+            <br/>
+            Bet ${bet}
+            <br/>
+            <button>+</button>
+            <button>-</button>
+            <br/>
+            <button style={{float:"left"}}>+</button>
+            <br/>
+            <div style={{float:"left", margin: "0", position: "relative", left: "-25px"}}> Unit ${unit}</div>
+            <br/>
+            <button style={{float: "left"}}>-</button>
+        </div>
+    );
+
+}
+
+const Dealer = () =>{
+
+}
+
+const Player = () =>{
+
+
+}
+
+const Game = () =>{
+
+    return(
+        <div>
+            <Status/>
+            {/*<Dealer/>*/}
+            {/*<Player/>*/}
+        </div>
+    )
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Game />
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
